73b5d2e11cc928c1115f5e8cf05e8bc1ad8e07e6
Victor Audetat <audetat.v@gmail.com>
2023-06-27 13:47:45 +0200

    clean useless did files

--- src/agents/declarations/NFTs/GOLD_10g_NFT/index.js
+++ src/agents/declarations/NFTs/GOLD_10g_NFT/index.js
@@ -1,6 +1,6 @@
 import { NFT_10_CANISTER_ID } from "../../../../constant.js";
 import { Actor } from "@dfinity/agent";
-import { idlFactory } from "./Gold_10_NFT.did.js";
+import { idlFactory } from "../Gold_NFT.did.js";
 export { idlFactory };
 
 export const canisterId = NFT_10_CANISTER_ID
--- src/agents/declarations/NFTs/GOLD_1g_NFT/index.js
+++ src/agents/declarations/NFTs/GOLD_1g_NFT/index.js
@@ -1,6 +1,6 @@
 import { NFT_1_CANISTER_ID } from "../../../../constant.js";
 import { Actor } from "@dfinity/agent";
-import { idlFactory } from "./Gold_1_NFT.did.js";
+import { idlFactory } from "../Gold_NFT.did.js";
 export { idlFactory };
 
 export const canisterId = NFT_1_CANISTER_ID
