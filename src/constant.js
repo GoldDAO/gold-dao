404b8730767413b0c6ffbc7a5bd587cdd7f10f4f
Victor Audetat <audetat.v@gmail.com>
2023-06-28 10:51:43 +0200

    fix: app disconnect no principalID

--- src/constant.js
+++ src/constant.js
@@ -2,3 +2,8 @@ export const NFT_1_CANISTER_ID = "obapm-2iaaa-aaaak-qcgca-cai"
 export const NFT_10_CANISTER_ID = "xyo2o-gyaaa-aaaal-qb55a-cai"
 export const NFT_100_CANISTER_ID = "zhfjc-liaaa-aaaal-acgja-cai"
 export const NFT_1000_CANISTER_ID = "7i7jl-6qaaa-aaaam-abjma-cai"
+
+export const GLDT_CANISTER_ID = "m45be-jaaaa-aaaak-qcgnq-cai"
+
+export const ICP_LEDGER_CANISTER_ID = 'ryjl3-tyaaa-aaaaa-aaaba-cai'
+export const YUMI_KYC_CANISTER_ID = 'ucs6g-wiaaa-aaaah-abwpa-cai'
