f1853c364f5adb825e603b106d23e8dfd963a79c
Fabio Bonfiglio <fabio.bonfiglio@protonmail.ch>
2023-08-14 10:27:56 +0200

    Adapt external canisters id import

--- client/next.config.js
+++ client/next.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 const env = require('dotenv').config({
   path: path.resolve(__dirname, './../.env'),
 });
-const CanisterIds = require('./../canister_ids.json');
+const ExternalCanisterIds = require('./../dfx.json');
 
 const canisterKey = process.env.DFX_NETWORK || 'staging'; // takes "staging" for develop and staging environment and "ic" for production environment
 const GLDNFT_CANISTER_IDS = {
-  '1g': CanisterIds.gldnft_backend_1g[canisterKey],
-  '10g': CanisterIds.gldnft_backend_10g[canisterKey],
-  '100g': CanisterIds.gldnft_backend_100g[canisterKey],
-  '1000g': CanisterIds.gldnft_backend_1000g[canisterKey],
+  '1g': ExternalCanisterIds.canisters.gldnft_backend_1g.remote.id[canisterKey],
+  '10g': ExternalCanisterIds.canisters.gldnft_backend_10g.remote.id[canisterKey],
+  '100g': ExternalCanisterIds.canisters.gldnft_backend_100g.remote.id[canisterKey],
+  '1000g': ExternalCanisterIds.canisters.gldnft_backend_1000g.remote.id[canisterKey],
 };
 
 const nextConfig = {
@@ -21,8 +21,8 @@ const nextConfig = {
   },
   env: {
     GLDNFT_CANISTER_IDS,
-    YUMI_KYC_CANISTER_ID: CanisterIds.yumi_kyc.ic,
-    ICP_LEDGER_CANISTER_ID: CanisterIds.icp_ledger.ic,
+    YUMI_KYC_CANISTER_ID: ExternalCanisterIds.canisters.yumi_kyc.remote.id.ic,
+    ICP_LEDGER_CANISTER_ID: ExternalCanisterIds.canisters.icp_ledger.remote.id.ic,
     DFX_NETWORK: process.env.DFX_NETWORK,
   },
   images: { unoptimized: true },
