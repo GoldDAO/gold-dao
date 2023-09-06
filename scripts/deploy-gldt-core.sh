927904eaaa21245a8b57421c1682542a6f5776f4
Dustin <dustin@bochslerfinance.com>
2023-09-06 14:33:30 +0200

    test commit for gpg signing

--- scripts/deploy-gldt-core.sh
+++ scripts/deploy-gldt-core.sh
@@ -6,6 +6,7 @@ dfx deploy gldt_core --network ${NETWORK} --argument '(
   opt record {gldt_ledger_canister_id=principal "'"$(dfx canister id --network ${NETWORK} gldt_ledger)"'";
   gld_nft_canister_ids=vec{
     record { principal "'"$(dfx canister id --network ${NETWORK} gldnft_backend_1g)"'"; record { grams=1}};
-    record { principal "'"$(dfx canister id --network ${NETWORK} gldnft_backend_10g)"'"; record { grams=10}};
-    record { principal "'"$(dfx identity get-principal)"'"; record { grams=100}};
+    record { principal "'"$(dfx canister id --network ${NETWORK} gldnft_backend_10g)"'"; record { grams=10}}
     }})' --mode reinstall
+
+    # record { principal "'"$(dfx identity get-principal)"'"; record { grams=100}};
