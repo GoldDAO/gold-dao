88479aeed129c351b2b980df6769ad36a7e4f838
Fabio Bonfiglio <fabio.bonfiglio@protonmail.ch>
2023-09-15 18:03:30 +0200

    Build scripts - Use canister build script - Generate candid and declarations from script - Build frontend after candid and declarations generation

--- scripts/generate-did.sh
+++ scripts/generate-did.sh
@@ -1,27 +1,72 @@
 #!/usr/bin/env bash
+: << EOF
+=pod
 
-# this only works with ic-cdk version 0.10.0 or higher. Due to crate compatibility issues with candid, I had to
-# downgrade and this is not working at the moment (Dustin, 24.08.23)
+=head1 NAME
 
-# call this from the root directly of gldt_swap project like `./scripts/generate-did.sh`
+Generate the candid file and declarations for the mentioned canister.
 
-function generate_did() {
-  local canister=$1
-  did_path="canister/$canister/src"
+=head1 SYNOPSYS
 
-  cargo build --target wasm32-unknown-unknown \
-      --release --package "$canister" \
-      --features "ic-cdk/wasi"
+generate-did [options] canister
 
-  # Installation https://docs.wasmtime.dev/cli-install.html
-  wasmtime "target/wasm32-unknown-unknown/release/$canister.wasm" > "$did_path/$canister.did"
-  dfx generate $canister
+=head1 OPTIONS
+
+-h, --help
+  Show this message and exit
+
+-o, --output PATH
+  The path where to write the resulting candid file (Must be a folder)
+
+-d, --dry-run
+  Only output the result, without actually writing on disk
+
+=cut
+
+EOF
+
+show_help() {
+  cat << EOF
+Generate the candid file and declarations for the mentioned canister.
+Usage:
+  generate-did [options] canister
+
+Options:
+  -h, --help        Show this message and exit
+  -o, --output PATH The path where to write the resulting candid file (Must be a folder)
+  -d, --dry-run     Only output the result, without actually writing on disk
+EOF
 }
 
-# The list of canisters of your project, comma separated, e.g. CANISTERS=gldt_core,gldt_ledger,...
-CANISTERS=gldt_core
+if [[ $# -gt 0 ]]; then
+  while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do
+    case $1 in
+      -h | --help )
+        show_help
+        exit
+        ;;
+      -o | --output )
+        shift; outpath=$1
+        ;;
+      -d | --dry-run )
+        dryrun=1
+        ;;
+    esac;
+    shift;
+  done
+  if [[ "$1" == '--' ]]; then shift; fi
+else
+  echo "Error: not enough arguments."
+  show_help
+  exit 1
+fi
 
-for canister in $(echo $CANISTERS | sed "s/,/ /g")
-do
-    generate_did "$canister"
-done
+defaultpath="canister/$1/src"
+did_path="${outpath:-$defaultpath}"
+if [[ $dryrun -eq 1 ]]; then
+  echo "This would be written to ${did_path}/${1}.did :"
+  wasmtime "canister/$1/target/wasm32-unknown-unknown/release/${1}_canister.wasm"
+else
+  wasmtime "canister/$1/target/wasm32-unknown-unknown/release/${1}_canister.wasm" > "$did_path/$1.did" &&
+  dfx generate $1
+fi
