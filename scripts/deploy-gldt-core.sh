bd67e4cd261e4fedd5ae12b9990d420175226b77
Fabio Bonfiglio <fabio.bonfiglio@protonmail.ch>
2023-09-20 17:14:51 +0200

    Prepare gldt_ledger and gldt_core deploy scripts

--- scripts/deploy-gldt-core.sh
+++ scripts/deploy-gldt-core.sh
@@ -1,12 +1,24 @@
-# Change the variable to "ic" to deploy the ledger on the mainnet.
-export NETWORK=staging
+#!/usr/bin/env bash
 
+## As argument, preferably pass $1 previously defined by calling the pre-deploy script with the dot notation.
 
-dfx deploy gldt_core --network ${NETWORK} --argument '(
-  opt record {gldt_ledger_canister_id=principal "'"$(dfx canister id --network ${NETWORK} gldt_ledger)"'";
+if [[ $1 !~ ^(local|staging|ic)$ ]]; then
+  echo "Error: unknown network for deployment"
+  exit 1
+fi
+
+if [[ $1 == "local" ]]; then
+  dfx deploy gldt_core --network $1 --argument '(
+  opt record {gldt_ledger_canister_id=principal "'"$(dfx canister id --network ${1} gldt_ledger)"'";
   gld_nft_canister_ids=vec{
-    record { principal "'"$(dfx canister id --network ${NETWORK} gldnft_backend_1g)"'"; record { grams=1}};
-    record { principal "'"$(dfx canister id --network ${NETWORK} gldnft_backend_10g)"'"; record { grams=10}}
-    }})' --mode reinstall
+    record { principal "'"$(dfx canister id --network ${1} gldnft_backend_1g)"'"; record { grams=1}};
+    record { principal "'"$(dfx canister id --network ${1} gldnft_backend_10g)"'"; record { grams=10}}
+    }})' --mode reinstall -y
+elif [[ $CI_COMMIT_REF_NAME == "develop" || $CI_COMMIT_TAG =~ ^core-v{1}[[:digit:]]{1,2}.[[:digit:]]{1,2}.[[:digit:]]{1,3}$ ]]; then
+  dfx deploy gldt_core --network $1 --argument '(
+    opt record {gldt_ledger_canister_id=principal "'"$(dfx canister id --network ${1} gldt_ledger)"'";
+    gld_nft_canister_ids=vec{
+      record { principal "'"$(dfx canister id --network ${1} gldnft_backend_1g)"'"; record { grams=1}};
+      record { principal "'"$(dfx canister id --network ${1} gldnft_backend_10g)"'"; record { grams=10}}
+      }})' --compute-evidence -y
 
-    # record { principal "'"$(dfx identity get-principal)"'"; record { grams=100}};
--- scripts/deploy-ledger.sh
+++ scripts/deploy-ledger.sh
@@ -1,9 +1,15 @@
-# Change the variable to "ic" to deploy the ledger on the mainnet.
-export NETWORK=staging
+#!/usr/bin/env bash
+
+## As argument, preferably pass $1 previously defined by calling the pre-deploy script with the dot notation.
+
+if [[ ! $1 =~ ^(local|staging|ic)$ ]]; then
+  echo "Error: unknown network for deployment"
+  exit 1
+fi
 
 # Change the variable to the account that can mint and burn tokens.
-# export MINT_ACC=$(dfx ledger account-id --network ${NETWORK} --of-canister gldt_core)
-export MINT_ACC=$(dfx canister id --network ${NETWORK} gldt_core)
+# export MINT_ACC=$(dfx ledger account-id --network ${1} --of-canister gldt_core)
+export MINT_ACC=$(dfx canister id gldt_core --network $1)
 
 # Change the variable to the principal that controls archive canisters.
 export ARCHIVE_CONTROLLER=$(dfx identity get-principal)
@@ -11,30 +17,28 @@ export ARCHIVE_CONTROLLER=$(dfx identity get-principal)
 export TOKEN_NAME="Gold token"
 export TOKEN_SYMBOL="GLDT"
 
-dfx deploy --network ${NETWORK} gldt_ledger --argument "(variant {Init = record {
-  token_name = \"${TOKEN_NAME}\";
-  token_symbol = \"${TOKEN_SYMBOL}\";
-  minting_account = record {owner = principal \"${MINT_ACC}\"};
-  metadata = vec {};
-  transfer_fee = 10000;
-  initial_balances = vec {};
-  archive_options = record {
-    trigger_threshold = 2000;
-    num_blocks_to_archive = 1000;
-    controller_id = principal \"${ARCHIVE_CONTROLLER}\";
-    cycles_for_archive_creation = opt 10_000_000_000_000;
-  }
-}})"
-
-
-
-# # to deploy rosetta API
-# docker run \
-#     --interactive \
-#     --tty \
-#     --publish 8081:8080 \
-#     --rm \
-#     dfinity/rosetta-api:latest \
-#     --canister-id 6uad6-fqaaa-aaaam-abovq-cai \
-#     --ic-url "https://icp0.io" \
-#     -t GLDT
+if [[ $1 == "local" ]]; then
+  dfx deploy --network $1 gldt_ledger --argument "(variant {Init = record {
+    token_name = \"${TOKEN_NAME}\";
+    token_symbol = \"${TOKEN_SYMBOL}\";
+    minting_account = record {owner = principal \"${MINT_ACC}\"};
+    metadata = vec {};
+    transfer_fee = 10000;
+    initial_balances = vec {};
+    archive_options = record {
+      trigger_threshold = 2000;
+      num_blocks_to_archive = 1000;
+      controller_id = principal \"${ARCHIVE_CONTROLLER}\";
+      cycles_for_archive_creation = opt 10_000_000_000_000;
+    }
+  }})"
+elif [[ $CI_COMMIT_REF_NAME == "develop" ]]; then
+  echo "TODO: Continue with ledger deployment for staging"
+  exit 1
+elif [[ $CI_COMMIT_TAG =~ ^ledger-v{1}[[:digit:]]{1,2}.[[:digit:]]{1,2}.[[:digit:]]{1,3}$ ]]; then
+  echo "TODO: Continue with ledger deployment for staging"
+  exit 1
+else
+  echo "Error: no valid deployment conditions found."
+  exit 1
+fi
