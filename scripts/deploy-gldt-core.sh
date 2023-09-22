c22797014b72ee04862556f7b007bfa44f844115
Fabio Bonfiglio <fabio.bonfiglio@protonmail.ch>
2023-09-22 15:20:52 +0200

    Document script and error message

--- scripts/deploy-gldt-core.sh
+++ scripts/deploy-gldt-core.sh
@@ -2,9 +2,44 @@
 
 ## As argument, preferably pass $1 previously defined by calling the pre-deploy script with the dot notation.
 
+show_help() {
+  cat << EOF
+gldt_core canister deployment script.
+Must be run from the repository's root folder, and with a running replica if for local deployment.
+'staging' and 'ic' networks can only be selected from a Gitlab CI/CD environment.
+The NETWORK argument should preferably be passed from the env variable that was previously defined
+by the pre-deploy script (using the dot notation, or inside a macro deploy script).
+
+The canister will always be reinstalled locally, and only upgraded in staging and production (ic).
+
+Usage:
+  scripts/deploy-ledger [options] <NETWORK>
+
+Options:
+  -h, --help        Show this message and exit
+EOF
+}
+
+# TODO: add a --identity option ?? (See dfx deploy --identity)
+if [[ $# -gt 0 ]]; then
+  while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do
+    case $1 in
+      -h | --help )
+        show_help
+        exit
+        ;;
+    esac;
+    shift;
+  done
+  if [[ "$1" == '--' ]]; then shift; fi
+else
+  echo "Error: missing <NETWORK> argument"
+  exit 1
+fi
+
 if [[ ! $1 =~ ^(local|staging|ic)$ ]]; then
   echo "Error: unknown network for deployment"
-  exit 1
+  exit 2
 fi
 
 if [[ $1 == "local" ]]; then
