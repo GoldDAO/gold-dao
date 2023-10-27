88363c66b5fee07379927dd7ae43c6e10b20fb6d
Fabio Bonfiglio <fabio.bonfiglio@protonmail.ch>
2023-10-27 11:51:25 +0200

    Prepare for gldt-fee-compensation canister deploy

--- scripts/local-deploy.sh
+++ scripts/local-deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 . scripts/pre-deploy.sh local && \
-echo "Deploying gldt-ledger on ${NETWORK}" && \
+echo "Deploying gldt_ledger on ${NETWORK}" && \
 . scripts/deploy-ledger.sh $NETWORK && \
-echo "Deploying gldt-core on ${NETWORK}" && \
+echo "Deploying gldt_fee_compensation on ${NETWORK}" && \
+. scripts/deploy-gldt-fee-compensation.sh $NETWORK && \
+echo "Deploying gldt_core on ${NETWORK}" && \
 . scripts/deploy-gldt-core.sh $NETWORK
