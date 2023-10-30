75ce271b5282c6d7d947b397060294c807b7daee
Fabio Bonfiglio <fabio.bonfiglio@protonmail.ch>
2023-10-30 16:49:07 +0100

    Add gldt-ledger-indexer deployment

--- scripts/local-deploy.sh
+++ scripts/local-deploy.sh
@@ -3,6 +3,8 @@
 . scripts/pre-deploy.sh local && \
 echo "Deploying gldt_ledger on ${NETWORK}" && \
 . scripts/deploy-ledger.sh $NETWORK && \
+echo "Deploying gldt_ledger_indexer on ${NETWORK}" && \
+. scripts/deploy-ledger-indexer.sh $NETWORK && \
 echo "Deploying gldt_fee_compensation on ${NETWORK}" && \
 . scripts/deploy-gldt-fee-compensation.sh $NETWORK && \
 echo "Deploying gldt_core on ${NETWORK}" && \
