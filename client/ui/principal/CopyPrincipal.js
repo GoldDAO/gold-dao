63a27055360222617a15dab754e827f0254e5eeb
Victor Audetat <victor@bochslerfinance.com>
2023-11-16 15:21:19 +0100

    fix explorer and transfer improvement

--- client/ui/principal/CopyPrincipal.js
+++ client/ui/principal/CopyPrincipal.js
@@ -3,31 +3,31 @@ import { Button, useToast } from '@chakra-ui/react';
 import React, { useState } from 'react';
 
 const CopyPrincipal = ({ text }) => {
-    const toast = useToast({
-        position: 'bottom',
-    });
-    return (
-        <Button
-            borderRadius={'200px'}
-            _hover={{
-                bg: 'bg',
-            }}
-            h={'40px'}
-            w={'40px'}
-            bg="transparent"
-            color="black"
-            aria-label="Copy to clipboard"
-            onClick={() => {
-                navigator.clipboard.writeText(text);
-                toast({
-                    title: 'Success',
-                    description: 'Principal copied to clipboard',
-                });
-            }}
-        >
-            <CopyIcon />
-        </Button>
-    );
+	const toast = useToast({
+		position: 'bottom',
+	});
+	return (
+		<Button
+			borderRadius={'200px'}
+			_hover={{
+				bg: 'bg',
+			}}
+			h={'40px'}
+			w={'40px'}
+			bg="transparent"
+			color="black"
+			aria-label="Copy to clipboard"
+			onClick={() => {
+				navigator.clipboard.writeText(text);
+				toast({
+					title: 'Success',
+					description: 'Principal copied to clipboard',
+				});
+			}}
+		>
+			<CopyIcon />
+		</Button>
+	);
 };
 
 export default CopyPrincipal;
