fd0ed4851a8487e351aa58082fd8c04122c16c51
Fabio Bonfiglio <fabio.bonfiglio@protonmail.ch>
2023-11-30 20:50:11 +0100

    Use dockerized sns-testing and automate deployment

--- scripts/sns-tests.sh
+++ scripts/sns-tests.sh
@@ -1,3 +1,28 @@
 #!/usr/bin/env bash
 
-# TODO: implement script according to https://gitlab.bochslerfinance.com/gldt/gldt-swap/-/issues/117#note_5906
+SNS_TESTING_INSTANCE=$(
+	docker run -p 8000:8000 -p 8080:8080 -v "`pwd`":/dapp -d ghcr.io/dfinity/sns-testing:main dfx start --clean
+)
+while ! docker logs $SNS_TESTING_INSTANCE 2>&1 | grep -m 1 'Dashboard:'
+do
+	echo "Awaiting local replica ..."
+	sleep 3
+done
+
+docker exec -it $SNS_TESTING_INSTANCE bash setup_locally.sh
+
+case "$(uname -s)" in
+	Linux*)
+		xdg-open http://qsgjb-riaaa-aaaaa-aaaga-cai.localhost:8080/;;
+	Darwin*)
+		open http://qsgjb-riaaa-aaaaa-aaaga-cai.localhost:8080/;;
+	*)
+		echo "Unrecognized system. Visit the local NNS frontend in your browser: http://qsgjb-riaaa-aaaaa-aaaga-cai.localhost:8080/"
+esac
+
+
+docker exec -it $SNS_TESTING_INSTANCE bash "cd /dapp && NETWORK='local' scripts/local-deploy.sh"
+
+docker exec -it $SNS_TESTING_INSTANCE bash
+
+docker kill $SNS_TESTING_INSTANCE
