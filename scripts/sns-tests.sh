b40ed4108d54cac1d949baf00e31f04f25394e7c
Fabio Bonfiglio <fabio.bonfiglio@protonmail.ch>
2023-12-06 11:33:55 +0100

    Adapt for manual deploy from inside docker

--- scripts/sns-tests.sh
+++ scripts/sns-tests.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Using host identiy $(dfx identity whoami) to replicate id on dockerized environement."
-
-NETNAME="snstesting"
-
 SNS_TESTING_INSTANCE=$(
 	docker run -p 8000:8000 -p 8080:8080 -v "`pwd`":/dapp -d ghcr.io/dfinity/sns-testing:main dfx start --clean --artificial-delay 100
 )
@@ -13,22 +9,9 @@ do
 	sleep 3
 done
 
-WALLETID=$(dfx identity get-wallet --network ${NETNAME})
-#dfx identity deploy-wallet --network $NETNAME $WALLETID
-dfx identity export $(dfx identity whoami) > tmp_id.pem
-
-docker exec -it $SNS_TESTING_INSTANCE dfx identity import -v --storage-mode plaintext --force default /dapp/tmp_id.pem
-rm tmp_id.pem
-#docker exec -it $SNS_TESTING_INSTANCE dfx identity set-wallet -v ${WALLETID}
-
-docker exec -it $SNS_TESTING_INSTANCE bash setup_locally.sh
-
 curl --progress-bar -o scripts/nvm_install.sh https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.6/install.sh
 docker exec -it $SNS_TESTING_INSTANCE bash /dapp/scripts/nvm_install.sh
 rm scripts/nvm_install.sh
-docker exec -it $SNS_TESTING_INSTANCE bash "nvm install 18"
-
-NETWORK="snstesting" scripts/local-deploy.sh
 
 case "$(uname -s)" in
 	Linux*)
@@ -40,6 +23,23 @@ case "$(uname -s)" in
 	*)
 		echo "Unrecognized system. Visit the local NNS frontend in your browser: http://qsgjb-riaaa-aaaaa-aaaga-cai.localhost:8080/"
 esac
+
+	cat << EOF
+The dockerized replica should now be running with the NNS and SNS canisters deployed,
+which should be listed on the dashboard: http://localhost:8000/_/dashboard
+
+You should now finish to setup the node environment and deploy the Gold canisters manually:
+
+nvm install 18
+cd /dapp
+npm install && npm run build && npm run deploy
+
+Then you should be able to interact with your canisters with dfx from the /dapp folder, and
+the NNS and SNS canisters with dfx from the ~/ ($HOME) folder.
+
+Type 'exit' when you are finished.
+
+EOF
 
 docker exec -it $SNS_TESTING_INSTANCE bash
 
