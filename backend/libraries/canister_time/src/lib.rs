f9af600ef51c0ac88d28e2a7f7467cc94b63b416
Dustin Becker <dustin@bochslerfinance.com>
2024-02-05 13:35:15 +0100

    extended canister_time library

--- backend/libraries/canister_time/src/lib.rs
+++ backend/libraries/canister_time/src/lib.rs
@@ -1,3 +1,15 @@
+use std::time::Duration;
+
+use types::{ Milliseconds, TimestampMillis, TimestampNanos };
+
+pub const SECOND_IN_MS: Milliseconds = 1000;
+pub const MINUTE_IN_MS: Milliseconds = SECOND_IN_MS * 60;
+pub const HOUR_IN_MS: Milliseconds = MINUTE_IN_MS * 60;
+pub const DAY_IN_MS: Milliseconds = HOUR_IN_MS * 24;
+pub const WEEK_IN_MS: Milliseconds = DAY_IN_MS * 7;
+
+pub const NANOS_PER_MILLISECOND: u64 = 1_000_000;
+
 pub fn timestamp_seconds() -> u64 {
     timestamp_nanos() / 1_000_000_000
 }
@@ -20,4 +32,17 @@ pub fn timestamp_nanos() -> u64 {
     use std::time::SystemTime;
 
     SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_nanos() as u64
+}
+
+pub fn now_millis() -> TimestampMillis {
+    now_nanos() / NANOS_PER_MILLISECOND
+}
+
+pub fn now_nanos() -> TimestampNanos {
+    ic_cdk::api::time()
+}
+
+pub fn run_now_then_interval(interval: Duration, func: fn()) {
+    ic_cdk_timers::set_timer_interval(interval, func);
+    ic_cdk_timers::set_timer(Duration::ZERO, func);
 }
