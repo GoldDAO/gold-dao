38b973fe4157cd392ef4d6032660f631332e57ea
Dustin Becker <dustin@bochslerfinance.com>
2024-02-22 17:46:45 +0100

    made candid available

--- backend/canisters/icp_neuron/impl/src/queries/mod.rs
+++ backend/canisters/icp_neuron/impl/src/queries/mod.rs
@@ -1,2 +1,9 @@
+use ic_cdk::query;
+
 mod http_request;
 mod transform_http_response;
+
+#[query(hidden = true)]
+fn __get_candid_interface_tmp_hack() -> String {
+    include_str!("../../../api/can.did").to_string()
+}
