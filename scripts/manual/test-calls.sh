3dc94faddfd7ff20891b477f568bb88e316a9c52
Dustin Becker <dustin@bochslerfinance.com>
2024-02-23 09:37:47 +0100

    added more test calls in shell sript

--- scripts/manual/test-calls.sh
+++ scripts/manual/test-calls.sh
@@ -2,3 +2,9 @@
 
 dfx canister call --network staging icp_neuron manage_nns_neuron_validate '(record { neuron_id = 123;
 command= variant{ Spawn = record {percentage_to_spawn=null; new_controller=null; nonce=null }} })'
+
+
+dfx canister call --network staging icp_neuron stake_nns_neuron
+
+dfx canister call --network staging icp_neuron manage_nns_neuron '(record {neuron_id = 17481076647658761488;
+command = variant { Configure = record { operation = opt variant { AddHotKey = record { new_hot_key = opt principal "j2neh-vqaaa-aaaal-aduxq-cai" } }} }})'
