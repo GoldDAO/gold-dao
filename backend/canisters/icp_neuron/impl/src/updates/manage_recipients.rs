7de94a2f113ba05a4d4ff11648619fbc5a1f0d6c
Dustin Becker <dustin@bochslerfinance.com>
2024-02-27 16:03:04 +0100

    hid manage_recipients method until implemented

--- backend/canisters/icp_neuron/impl/src/updates/manage_recipients.rs
+++ backend/canisters/icp_neuron/impl/src/updates/manage_recipients.rs
@@ -3,7 +3,7 @@ use canister_tracing_macros::trace;
 use ic_cdk::update;
 
 // method to add / remove recipients of reward distribution
-#[update(guard = "caller_is_governance_principal")]
+#[update(guard = "caller_is_governance_principal", hidden = true)]
 #[trace]
 async fn manage_recipients() -> () {
     manage_recipients_impl().await
