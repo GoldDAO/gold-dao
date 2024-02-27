68abc1041e53daba3bf5fc6d012b048c9da70fcd
Dustin Becker <dustin@bochslerfinance.com>
2024-02-27 17:29:53 +0100

    added another rebuild to manual deployment to ensure candid is updated

--- scripts/manual/deploy-icp-neuron.sh
+++ scripts/manual/deploy-icp-neuron.sh
@@ -2,6 +2,8 @@
 
 ./scripts/build-canister.sh icp_neuron &&
 ./scripts/generate-did.sh icp_neuron &&
+./scripts/build-canister.sh icp_neuron &&
+
 # dfx deploy --network staging icp_neuron --argument '(opt record {test_mode = true; rewards_recipients = vec {
 #   record { tag = "Team development"; reward_weight = 3300; account = record { owner = principal "465sx-szz6o-idcax-nrjhv-hprrp-qqx5e-7mqwr-wadib-uo7ap-lofbe-dae"; subaccount = null } };
 #   record { tag = "GLDGov voters"; reward_weight = 3300; account = record { owner = principal "2f5ll-gqaaa-aaaak-qcfuq-cai"; subaccount = null } };
