9f771e16c99d4a93b2e34368eccf354d9431df67
Dustin Becker <dustin@bochslerfinance.com>
2024-02-29 18:19:19 +0100

    added RefCell to MEMORY_MANAGER

--- backend/canisters/icp_neuron/impl/src/memory.rs
+++ backend/canisters/icp_neuron/impl/src/memory.rs
@@ -1,5 +1,7 @@
+use std::cell::RefCell;
+
 use ic_stable_structures::{
-    memory_manager::{MemoryId, MemoryManager, VirtualMemory},
+    memory_manager::{ MemoryId, MemoryManager, VirtualMemory },
     DefaultMemoryImpl,
 };
 
@@ -8,8 +10,9 @@ const UPGRADES: MemoryId = MemoryId::new(0);
 pub type Memory = VirtualMemory<DefaultMemoryImpl>;
 
 thread_local! {
-    static MEMORY_MANAGER: MemoryManager<DefaultMemoryImpl>
-        = MemoryManager::init_with_bucket_size(DefaultMemoryImpl::default(), 1);
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> = RefCell::new(
+        MemoryManager::init_with_bucket_size(DefaultMemoryImpl::default(), 1)
+    );
 }
 
 pub fn get_upgrades_memory() -> Memory {
@@ -17,5 +20,5 @@ pub fn get_upgrades_memory() -> Memory {
 }
 
 fn get_memory(id: MemoryId) -> Memory {
-    MEMORY_MANAGER.with(|m| m.get(id))
+    MEMORY_MANAGER.with(|m| m.borrow().get(id))
 }
