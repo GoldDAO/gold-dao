a5213ff878535aa37aac5e38e9dbbf5b9a1d1fa3
Fabio Bonfiglio <fabio.bonfiglio@protonmail.ch>
2024-03-06 22:13:57 +0100

    Add compute evidence for asset canisters

--- scripts/local-deploy.sh
+++ scripts/local-deploy.sh
@@ -4,6 +4,7 @@
 if [[ $? != 0 ]]; then
 	exit 1;
 fi
+
 echo "Deploying gldt_ledger on ${NETWORK}"
 . scripts/deploy-ledger.sh $NETWORK && \
 echo "Deploying gldt_ledger_indexer on ${NETWORK}" && \
@@ -13,6 +14,9 @@ echo "Deploying gldt_core on ${NETWORK}"
 echo "Deploying gldt_fee_compensation on ${NETWORK}"
 . scripts/deploy-gldt-fee-compensation.sh $NETWORK
 echo "Deploying frontends on ${NETWORK}"
+dfx deploy --network ${NETWORK} --compute-evidence gldt_landing_page
+dfx deploy --network ${NETWORK} --compute-evidence gldt_swap_app
+dfx deploy --network ${NETWORK} --compute-evidence gldt_explorer
 dfx deploy --network ${NETWORK} gldt_landing_page
 dfx deploy --network ${NETWORK} gldt_swap_app
 dfx deploy --network ${NETWORK} gldt_explorer
