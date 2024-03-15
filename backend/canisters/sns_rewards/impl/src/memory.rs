ff99a907e130e3bf5ca19fba034279a0e0054462
freddie <frederico02@hotmail.com>
2024-03-15 13:21:40 +0000

    refactor: remove memory for previously user_rewards state

--- backend/canisters/sns_rewards/impl/src/memory.rs
+++ backend/canisters/sns_rewards/impl/src/memory.rs
@@ -5,7 +5,6 @@ use ic_stable_structures::{
 
 const UPGRADES: MemoryId = MemoryId::new(0);
 const MATURITY_HISTORY: MemoryId = MemoryId::new(1);
-const USER_REWARD_SUB_ACCOUNT : MemoryId = MemoryId::new(2);
 
 // const EVENT_LOGS_INDEX_MEM_ID: MemoryId = MemoryId::new(1);
 // const EVENT_LOGS_DATA_MEM_ID: MemoryId = MemoryId::new(2);
@@ -24,10 +23,6 @@ pub fn get_upgrades_memory() -> VM {
 
 pub fn get_maturity_history_memory() -> VM {
     get_memory(MATURITY_HISTORY)
-}
-
-pub fn get_user_reward_accounts() -> VM {
-    get_memory(USER_REWARD_SUB_ACCOUNT)
 }
 
 fn get_memory(id: MemoryId) -> VM {
