69acae238158d652f8dcb97d47f51ef061960193
Dustin Becker <dustin@bochslerfinance.com>
2024-03-22 11:35:42 +0100

    added comment for migration

--- backend/canisters/icp_neuron/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/icp_neuron/impl/src/lifecycle/post_upgrade.rs
@@ -1,5 +1,6 @@
 use crate::lifecycle::init_canister;
 use crate::memory::get_upgrades_memory;
+// use crate::migrations::types::state::RuntimeStateV0;
 use crate::state::RuntimeState;
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
@@ -16,6 +17,14 @@ fn post_upgrade() {
     let (runtime_state, logs, traces): (RuntimeState, Vec<LogEntry>, Vec<LogEntry>) = serializer
         ::deserialize(reader)
         .unwrap();
+
+    // uncomment these lines if you want to do an upgrade with migration
+    // let (runtime_state_v0, logs, traces): (
+    //     RuntimeStateV0,
+    //     Vec<LogEntry>,
+    //     Vec<LogEntry>,
+    // ) = serializer::deserialize(reader).unwrap();
+    // let runtime_state = RuntimeState::from(runtime_state_v0);
 
     canister_logger::init_with_logs(runtime_state.env.is_test_mode(), logs, traces);
     init_canister(runtime_state);
