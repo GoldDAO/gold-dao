841528ef88818fcd9404605e6f1fad1d17ddf8d0
Dustin Becker <dustin@bochslerfinance.com>
2024-03-26 17:51:42 +0100

    parse_proposal_details now adds the changelog to the proposal text

--- scripts/parse_proposal_details.sh
+++ scripts/parse_proposal_details.sh
@@ -1,12 +1,31 @@
 #!/usr/bin/env bash
 
-if [[ -v $CI_COMMIT_TAG ]]; then
-	VERSION=$CI_COMMIT_TAG
-else
-	VERSION="!STAGINGTEST!"
+CANISTER_NAME="$1"
+
+if [[ -z $CANISTER_NAME ]]; then
+    echo "Error: CANISTER_NAME is not defined." >&2
+    exit 1
 fi
 
-if [[ -v $CI_COMMIT_SHORT_SHA ]]; then
+if [[ -n $CI_COMMIT_TAG ]]; then
+	VERSION="${CI_COMMIT_TAG#*-v}"
+	NAME="${CI_COMMIT_TAG%-v*}"
+
+	# Check if NAME matches CANISTER_NAME
+	# The script requires CANISTER_NAME to be defined for staging deployment and
+	# because CI_COMMIT_TAG are only set on master, we add this safety check that they match
+	if [[ "$NAME" != "$CANISTER_NAME" ]]; then
+			echo "Error: NAME extracted from CI_COMMIT_TAG ('$NAME') does not match CANISTER_NAME ('$CANISTER_NAME')." >&2
+			exit 1
+	fi
+
+	./scripts/parse_changelog.sh $CANISTER_NAME $VERSION
+else
+	VERSION="!STAGINGTEST!"
+	echo "No changelog for staging deployment" > CHANGELOG.md
+fi
+
+if [[ -n $CI_COMMIT_SHORT_SHA ]]; then
 	COMMITSHA=$CI_COMMIT_SHORT_SHA
 else
 	COMMITSHA="00000000"
@@ -15,5 +34,7 @@ fi
 DETAILSURLFIELD=$(cat proposal_template.md | grep "details_url")
 export DETAILS_URL=${DETAILSURLFIELD:13}
 sed "s/<<VERSIONTAG>>/${VERSION}/g" proposal_template.md > proposal.md && \
-sed -i "s/<<COMMITHASH>>/${COMMITSHA}/g" proposal.md
+sed -i '' "s/<<COMMITHASH>>/${COMMITSHA}/g" proposal.md && \
+sed -i '' "s/<<CANISTER>>/${CANISTER_NAME}/g" proposal.md && \
+cat CHANGELOG.md >> proposal.md
 return
