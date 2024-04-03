28d6586b618162542e8bd36f5b47a3ad19c951b8
freddie <frederico02@hotmail.com>
2024-04-03 10:40:40 +0100

    feat: hide __get_candid_interface_tmp_hack && comment out sync_rewards & sync_neurons

--- backend/canisters/sns_rewards/impl/src/updates/sync_neurons.rs
+++ backend/canisters/sns_rewards/impl/src/updates/sync_neurons.rs
@@ -1,9 +1,9 @@
-// use sns_governance_canister::types::ListNeuronsResponse;
-use crate::jobs::synchronise_neurons::run;
-use ic_cdk::update;
+// // use sns_governance_canister::types::ListNeuronsResponse;
+// use crate::jobs::synchronise_neurons::run;
+// use ic_cdk::update;
 
-// Only for development, remove after
-#[update(hidden = true)]
-async fn sync_neurons_manual_trigger() {
-    run()
-}
+// // Only for development, remove after
+// #[update(hidden = true)]
+// async fn sync_neurons_manual_trigger() {
+//     run()
+// }
