b1a5750cc4e8c76d3b199c9c10efcaf53bb110c7
freddie <frederico02@hotmail.com>
2024-04-09 09:54:32 +0300

    feat: begin work

--- backend/canisters/sns_rewards/impl/src/jobs/mod.rs
+++ backend/canisters/sns_rewards/impl/src/jobs/mod.rs
@@ -1,7 +1,9 @@
 pub mod synchronise_neurons;
 pub mod distribute_rewards;
+pub mod reserve_pool_distribution;
 
 pub(crate) fn start() {
     synchronise_neurons::start_job();
     distribute_rewards::start_job();
+    reserve_pool_distribution::start_job();
 }
