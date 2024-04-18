02f25712348e173096ee10d576a6ea4005dc1558
Freddie Hands <freddie@Freddies-MacBook-Pro.local>
2024-04-18 07:45:47 +0300

    chore: clean up a little

--- backend/integration_testing/src/tests/distribute_rewards.rs
+++ backend/integration_testing/src/tests/distribute_rewards.rs
@@ -40,7 +40,10 @@ fn test_distribute_rewards_happy_path() {
     let TestEnv { mut pic, controller, token_ledgers, mut sns, rewards } = env;
     let sns_gov_id = sns.sns_gov_id.clone();
 
-    // . check all reward pools have correct balance
+    // ********************************
+    // 1. Check all reward pools have a balance
+    // ********************************
+
     let reward_pool = Account {
         owner: rewards,
         subaccount: Some([
@@ -58,27 +61,31 @@ fn test_distribute_rewards_happy_path() {
     let gldgov_reward_pool_balance = balance_of(&pic, token_ledgers.gldgov_ledger_id, reward_pool);
     assert_eq!(gldgov_reward_pool_balance, Nat::from(100_000_000_000u64));
 
-    // simulate neuron maturity change & allow 1 day for synchronise_neurons to process the change
+    // ********************************
+    // 2. Distribute rewards - week 1
+    // ********************************
+
+    // increase maturity maturity
     sns.setup_week(&mut pic, controller, 2, sns_gov_id);
     pic.tick();
     pic.advance_time(Duration::from_secs(60 * 60 * 24)); // 1 day
     tick_n_blocks(&pic, 10);
 
-    // simulate distribute_rewards
+    // trigger the distribute rewards
     pic.advance_time(Duration::from_secs(60 * 60 * 148)); // 6 days & 1 hour - full week + 1 hour
     tick_n_blocks(&pic, 10);
     pic.advance_time(Duration::from_secs(60 * 3));
     sync_user_rewards(&mut pic, Principal::anonymous(), rewards, &());
     tick_n_blocks(&pic, 100);
-    // all neurons have the same accumulated maturity
+
+    // calculate expected payment for every neuron ( they all have the same maturity - see neuron_data )
     let fees = (sns.neuron_test_data.len() as u64) * 10_000 + 10_000;
-    let amount_to_distribute = (100_000_000_000u64 - fees) as f64;
+    let payment_round_pool_amount = (100_000_000_000u64 - fees) as f64;
     let total_maturity: f64 = ((sns.neuron_test_data.len() as u64) * 100_000u64) as f64;
     let percentage = (100_000 as f64) / total_maturity;
-    let expected_reward = (amount_to_distribute * percentage) as u64;
+    let expected_reward = (payment_round_pool_amount * percentage) as u64;
     assert_eq!(expected_reward, 9_999_989_000);
 
-    // get balance of single neuron
     let neuron_sub_account = Account {
         owner: rewards,
         subaccount: Some(
@@ -87,19 +94,23 @@ fn test_distribute_rewards_happy_path() {
     };
     let neuron_icp_balance = balance_of(&pic, token_ledgers.icp_ledger_id, neuron_sub_account);
     assert_eq!(neuron_icp_balance, expected_reward);
-
     pic.tick();
+
+    // ********************************
+    // 3. Distribute rewards - week 2
+    // ********************************
+
+    // increase maturity
     sns.setup_week(&mut pic, controller, 3, sns_gov_id);
     pic.advance_time(Duration::from_secs(60 * 60 * 24)); // 1 day
     tick_n_blocks(&pic, 4);
-    // add new rewards
-    // top up the reward pools again
     setup_reward_pools(&mut pic, controller, rewards, token_ledgers, 100_000_000_000u64);
     pic.tick();
     pic.tick();
     pic.advance_time(Duration::from_secs(60 * 60 * 148)); // 6 days & 1 hour - full week + 1 hour
     tick_n_blocks(&pic, 10);
     pic.advance_time(Duration::from_secs(60 * 3));
+    // trigger reward distribution
     sync_user_rewards(&mut pic, Principal::anonymous(), rewards, &());
     tick_n_blocks(&pic, 100);
 
@@ -111,20 +122,6 @@ fn test_distribute_rewards_happy_path() {
     };
     let neuron_icp_balance = balance_of(&pic, token_ledgers.icp_ledger_id, neuron_sub_account);
     assert_eq!(neuron_icp_balance, expected_reward * 2);
-    // let p = get_active_payment_rounds(&pic, Principal::anonymous(), rewards, &());
-    // println!("{:?}", p);
-    // let res = http_request(
-    //     &pic,
-    //     Principal::anonymous(),
-    //     rewards,
-    //     &(types::HttpRequest {
-    //         method: "GET".to_string(),
-    //         url: "/trace".to_string(),
-    //         headers: vec![],
-    //         body: ByteBuf::new(),
-    //     })
-    // );
-    // println!("{}", decode_http_bytes(res.body.into_vec()));
 }
 
 // assert_eq!(true, false);
@@ -178,3 +175,19 @@ fn test_distribute_rewards_happy_path() {
 //     &NeuronId::new("146ed81314556807536d74005f4121b8769bba1992fce6b90c2949e855d04208").unwrap()
 // ).unwrap();
 // assert_eq!(single_neuron.accumulated_maturity, 200_000);
+
+// let res = http_request(
+//     &pic,
+//     Principal::anonymous(),
+//     rewards,
+//     &(types::HttpRequest {
+//         method: "GET".to_string(),
+//         url: "/trace".to_string(),
+//         headers: vec![],
+//         body: ByteBuf::new(),
+//     })
+// );
+// println!("{}", decode_http_bytes(res.body.into_vec()));
+
+// let p = get_active_payment_rounds(&pic, Principal::anonymous(), rewards, &());
+// println!("{:?}", p);
