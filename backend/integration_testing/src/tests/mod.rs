17daba3203f579bc82ec9aca90745577597d51f5
Freddie Hands <freddie@Freddies-MacBook-Pro.local>
2024-04-19 14:58:56 +0300

    chore: change name of test files

--- backend/integration_testing/src/tests/mod.rs
+++ backend/integration_testing/src/tests/mod.rs
@@ -1,4 +1,4 @@
-pub mod synchronise_neurons;
-pub mod distribute_rewards;
-// pub mod reserve_pool_distribution;
-pub mod reward_claim;
+pub mod test_synchronise_neurons;
+pub mod test_distribute_rewards;
+// pub mod test_reserve_pool_distribution;
+pub mod test_reward_claim;
