c1d57bed8f0d7c74daa1b7e5921dc64a15bfb348
Freddie Hands <freddie@Freddies-MacBook-Pro.local>
2024-04-19 15:18:45 +0300

    chore: comment out manual trigger sync_user_rewards

--- backend/canisters/sns_rewards/impl/src/updates/sync_user_rewards.rs
+++ backend/canisters/sns_rewards/impl/src/updates/sync_user_rewards.rs
@@ -1,9 +1,9 @@
-// use sns_governance_canister::types::ListNeuronsResponse;
-use crate::jobs::distribute_rewards::run_distribution;
-use ic_cdk::update;
+// // use sns_governance_canister::types::ListNeuronsResponse;
+// use crate::jobs::distribute_rewards::run_distribution;
+// use ic_cdk::update;
 
-// Only for development, remove after
-#[update(hidden = true)]
-async fn sync_user_rewards() {
-    run_distribution()
-}
+// // Only for development, remove after
+// #[update(hidden = true)]
+// async fn sync_user_rewards() {
+//     run_distribution()
+// }
