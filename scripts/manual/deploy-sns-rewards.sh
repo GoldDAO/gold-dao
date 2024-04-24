94e9cb82999ecf0543edf4b1317ff163656ae9a3
Dustin Becker <dustin@bochslerfinance.com>
2024-04-24 13:24:12 +0200

    small adjustment to manual deployment scripts

--- scripts/manual/deploy-sns-rewards.sh
+++ scripts/manual/deploy-sns-rewards.sh
@@ -2,5 +2,12 @@
 
 ./scripts/build-canister.sh sns_rewards &&
 ./scripts/generate-did.sh sns_rewards &&
+./scripts/build-canister.sh sns_rewards &&
 # dfx deploy --network staging sns_rewards --argument '(record {test_mode = true})' --mode reinstall
-dfx deploy --network staging sns_rewards --argument '(record {test_mode = true})'
+dfx deploy --network staging sns_rewards --argument "(record {
+  test_mode = true;
+  icp_ledger_canister_id = principal \"ete3q-rqaaa-aaaal-qdlva-cai\";
+  sns_ledger_canister_id = principal \"irhm6-5yaaa-aaaap-ab24q-cai\";
+  ogy_ledger_canister_id = principal \"jwcfb-hyaaa-aaaaj-aac4q-cai\";
+  sns_gov_canister_id = principal \"j3ioe-7iaaa-aaaap-ab23q-cai\"
+  })" --mode reinstall
