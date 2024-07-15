2f7d59086e8796520f3df22b44eea24a82e0cc1c
aditsch <audran@daolink.ch>
2024-07-15 18:01:44 +0200

    add base gldt swap with routing

--- client/gldt_swap_app/src/main.tsx
+++ client/gldt_swap_app/src/main.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { colors as themeColors } from "@theme/preset";
+import AuthProvider from "@providers/Auth.tsx";
+import App from "./App";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <>
+    <Toaster
+      position="bottom-center"
+      reverseOrder={false}
+      toastOptions={{
+        duration: 3000,
+        style: {
+          background: themeColors.surface[2],
+          color: themeColors.content,
+        },
+        success: {
+          duration: 3000,
+        },
+        error: {
+          duration: 4000,
+        },
+      }}
+    />
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
+  </>
+);
