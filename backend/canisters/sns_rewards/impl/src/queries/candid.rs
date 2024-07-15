a0487f6baa1faedfccb2917f8166f794f1ee40c2
Victoria Horbunova⁩ <victoria@daolink.ch>
2024-07-15 09:33:53 +0000

    Update file candid.rs

--- backend/canisters/sns_rewards/impl/src/queries/candid.rs
+++ backend/canisters/sns_rewards/impl/src/queries/candid.rs
@@ -1,8 +1,5 @@
 use ic_cdk::query;
 
-/// This makes this Candid service self-describing, so that for example Candid UI, but also other
-/// tools, can seamlessly integrate with it. The concrete interface (method name etc.) is
-/// provisional, but works.
 #[query(hidden = true)]
 fn __get_candid_interface_tmp_hack() -> String {
     include_str!("../../../api/can.did").to_string()
