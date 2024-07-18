b9c9b7a1e5836f31d3f48a216d2ca40fd92281eb
Freddie Hands <freddie@daolink.ch>
2024-07-18 13:07:47 +0300

    refactor: improve migration

--- backend/canisters/sns_rewards/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/sns_rewards/impl/src/lifecycle/post_upgrade.rs
@@ -1,8 +1,10 @@
+use candid::Nat;
 use canister_logger::LogEntry;
 use canister_tracing_macros::trace;
 use ic_cdk_macros::post_upgrade;
 use stable_memory::get_reader;
 use tracing::info;
+use types::TimestampMillis;
 
 use crate::{ memory::get_upgrades_memory, state::RuntimeState, utils::TimeInterval };
 
@@ -19,19 +21,43 @@ fn post_upgrade() {
         .unwrap();
 
     // Migrations
-    runtime_state.data.reward_distribution_interval = Some(TimeInterval::default());
 
-    if runtime_state.data.reward_distribution_in_progress.is_some() {
+    if runtime_state.data.daily_gldgov_burn_rate.is_none() {
+        runtime_state.data.daily_gldgov_burn_rate = None;
+    } else {
+        runtime_state.data.daily_gldgov_burn_rate = runtime_state.data.daily_gldgov_burn_rate;
+    }
+
+    if runtime_state.data.last_daily_gldgov_burn.is_none() {
+        runtime_state.data.last_daily_gldgov_burn = None;
+    } else {
+        runtime_state.data.last_daily_gldgov_burn = runtime_state.data.last_daily_gldgov_burn;
+    }
+
+    if runtime_state.data.reward_distribution_interval.is_none() {
+        runtime_state.data.reward_distribution_interval = Some(TimeInterval::default());
+    } else {
+        runtime_state.data.reward_distribution_interval =
+            runtime_state.data.reward_distribution_interval;
+    }
+
+    if runtime_state.data.reward_distribution_in_progress.is_none() {
+        runtime_state.data.reward_distribution_in_progress = Some(false);
+    } else {
         runtime_state.data.reward_distribution_in_progress =
             runtime_state.data.reward_distribution_in_progress;
-    } else {
-        runtime_state.data.reward_distribution_in_progress = Some(false);
     }
-    runtime_state.data.neuron_sync_interval = Some(TimeInterval {
-        weekday: None,
-        start_hour: 9,
-        end_hour: 11,
-    });
+
+    if runtime_state.data.neuron_sync_interval.is_none() {
+        runtime_state.data.neuron_sync_interval = Some(TimeInterval {
+            weekday: None,
+            start_hour: 9,
+            end_hour: 11,
+        });
+    } else {
+        runtime_state.data.neuron_sync_interval = runtime_state.data.neuron_sync_interval;
+    }
+
     // End migrations
     canister_logger::init_with_logs(runtime_state.env.is_test_mode(), logs, traces);
     init_canister(runtime_state);
