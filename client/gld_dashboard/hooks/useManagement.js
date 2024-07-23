d5b2cd126714fe2ed2de933770cf69a2c79db373
Freddie Hands <freddie@daolink.ch>
2024-07-23 14:31:07 +0300

    unless prod, always set to true

--- client/gld_dashboard/hooks/useManagement.js
+++ client/gld_dashboard/hooks/useManagement.js
@@ -6,7 +6,9 @@ const useManagement = () => {
   const getMaintenanceMode = async () => {
     try {
       const result = await management.get_gld_dashboard_maintenance_mode();
-
+      if (process.env.ENV !== 'prod') {
+        return true;
+      }
       return result;
     } catch (err) {
       console.log('Failed to fetch `get_gld_dashboard_maintenance_mode`');
