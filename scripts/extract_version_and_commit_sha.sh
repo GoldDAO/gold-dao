85c0aacfe4a343a2f80459675ba6dbcf6ca69b5e
Dustin Becker <dustin@bochslerfinance.com>
2024-07-26 15:18:19 +0200

    ci: cleanup and refactor

--- scripts/extract_version_and_commit_sha.sh
+++ scripts/extract_version_and_commit_sha.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-
-if [[ $# -ne 1 ]]; then
-  echo "Error: missing <CANISTER-NAME> argument."
-  exit 1
-fi
-
 CANISTER_NAME=$1
 
 if [[ -n $CI_COMMIT_TAG ]]; then
@@ -20,18 +14,18 @@ if [[ -n $CI_COMMIT_TAG ]]; then
 			exit 1
 	fi
 
-	# ./scripts/parse_changelog.sh $CANISTER_NAME $VERSION
-	# exit_status=$? # Capture the exit status of the last command
-
-	# if [[ $exit_status -eq 1 ]]; then
-	# 		echo "Error: parse_changelog.sh exited with status 1." >&2
-	# 		exit 1
-	# fi
 else
 	VERSION="_STAGINGTEST_"
-	# echo "No changelog for staging deployment" > CHANGELOG.md
 fi
 
-export VERSION
+if [[ -n $CI_COMMIT_SHORT_SHA ]]; then
+	COMMIT_SHA=$CI_COMMIT_SHORT_SHA
+else
+	COMMIT_SHA="00000000"
+fi
 
-# return
+
+export VERSION
+export COMMIT_SHA
+
+return
