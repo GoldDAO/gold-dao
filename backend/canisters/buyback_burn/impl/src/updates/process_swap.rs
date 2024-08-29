e71c1cf2ceca12e4213f7a760167e24244bef11a
Victoria Horbunova <victoria@daolink.ch>
2024-08-30 00:46:26 +0200

    feat: add manual swap and burn

---
+++ backend/canisters/buyback_burn/impl/src/updates/process_swap.rs
@@ -0,0 +1,7 @@
+use crate::guards::caller_is_governance_principal;
+use ic_cdk::update;
+
+#[update(guard = "caller_is_governance_principal", hidden = true)]
+pub fn process_swap() {
+    crate::jobs::swap_tokens::run()
+}
