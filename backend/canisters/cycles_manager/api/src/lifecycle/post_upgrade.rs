137852ada2495f57f537394aa88919f6bb3ce677
Victoria Horbunova <victoria@daolink.ch>
2024-09-10 12:43:01 +0200

    feat: delete unused

--- backend/canisters/cycles_manager/api/src/lifecycle/post_upgrade.rs
+++
@@ -1,9 +0,0 @@
-use candid::CandidType;
-use serde::{Deserialize, Serialize};
-use types::BuildVersion;
-
-#[derive(CandidType, Serialize, Deserialize, Debug)]
-pub struct UpgradeArgs {
-    pub wasm_version: BuildVersion,
-    pub commit_hash: String,
-}
