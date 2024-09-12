faa9b457addba899e08e7ef54828f519f84dcc50
Victor Raguin <victor.raguin@live.fr>
2024-09-12 15:00:28 +0200

    ✨ : improve gold dao metadata

--- client/gld_landing_page/src/app/page.tsx
+++ client/gld_landing_page/src/app/page.tsx
@@ -10,6 +10,27 @@ export async function generateStaticParams() {
   title: "The GOLD DAO Website",
   description:
     "Gold DAO is a decentralized community dedicated to revolutionizing the way gold is owned and utilised. Join the DAO in harnessing the power of blockchain technology to create a more transparent and accessible gold market.",
+    openGraph: {
+      type: "website",
+      url: "https://www.gold-dao.org/",
+      title: "The GOLD DAO Website",
+      description: "Gold DAO is a decentralized community dedicated to revolutionizing the way gold is owned and utilised. Join the DAO in harnessing the power of blockchain technology to create a more transparent and accessible gold market.",
+      images: [
+        {
+          url: "https://www.gold-dao.org/static/illustrations/gold_dao.png",
+          width: 800,
+          height: 600,
+          alt: "The GOLD DAO Website",
+        },
+      ],
+    },
+  
+    twitter: {
+      card: "summary_large_image",
+      title: "The GOLD DAO Website",
+      description: "Gold DAO is a decentralized community dedicated to revolutionizing the way gold is owned and utilised. Join the DAO in harnessing the power of blockchain technology to create a more transparent and accessible gold market.",
+      images: ["https://www.gold-dao.org/static/illustrations/gold_dao.png"],
+    },
 };
   
   export default function Page({ params }: { params: { locale: string } }) {
