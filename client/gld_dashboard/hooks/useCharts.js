c21de7ab3ff26c2baff70b6b8e2406ed7e6055db
Freddie Hands <freddie@daolink.ch>
2024-09-24 16:27:18 +0200

    feat: add stakers chart

--- client/gld_dashboard/hooks/useCharts.js
+++ client/gld_dashboard/hooks/useCharts.js
@@ -8,6 +8,7 @@ export default create((set, get) => ({
   gldGovTreasury: { loading: true, data: [] },
   copyGldGovTreasury: { loading: true, data: [] },
   stakersData: { loading: true, data: [] },
+  copyStakersData: { loading: true, data: [] },
   setSelectedDistance: (selectedDistance) => {
     const distance = currentTimestamp() - selectedDistance;
     const filteredSupply = get().gldGovSupply.data.filter(
@@ -20,17 +21,23 @@ export default create((set, get) => ({
     );
     const copyGldGovTreasury = filterDates(filteredTreasury);
 
+    const filteredStakersData = get().stakersData.data.filter(
+      ({ label }) => new Date(label) >= new Date(distance * 1000),
+    );
+
+    const copyStakersData = filterDates(filteredStakersData);
+
     return set({
       selectedDistance,
       copyGldGovSupply: { loading: false, data: copyGldGovSupply },
       copyGldGovTreasury: { loading: false, data: copyGldGovTreasury },
+      copyStakersData: { loading: false, data: copyStakersData },
     });
   },
   setGldGovSupply: (data) => {
     const distance = currentTimestamp() - 86400 * 31 * 6; // 6 months in seconds;
     const filtered = data.filter(({ label }) => new Date(label) >= new Date(distance * 1000));
     const copyGldGovSupply = filterDates(filtered);
-    console.log(copyGldGovSupply);
 
     return set({
       gldGovSupply: { loading: false, data },
@@ -48,14 +55,21 @@ export default create((set, get) => ({
     });
   },
   setStakersMetrics: (data) => {
+    const distance = currentTimestamp() - 86400 * 31 * 6; // 6 months in seconds;
     const millisPerDay = 24 * 60 * 60 * 1000; // Number of milliseconds in a day
-
     const mappedData = data.map(([daysSinceEpoch, value]) => ({
-      label: new Date(daysSinceEpoch * millisPerDay).toISOString().split('T')[0],
-      value,
+      label: new Date(Number(daysSinceEpoch) * millisPerDay).toISOString().split('T')[0],
+      value: Number(value.balance) / 1e8,
     }));
+
+    console.log(mappedData);
+
+    const filtered = mappedData.filter(({ label }) => new Date(label) >= new Date(distance * 1000));
+    const copyStakersData = filterDates(filtered);
+    console.log(copyStakersData);
     return set({
       stakersData: { loading: false, data: mappedData },
+      copyStakersData: { loading: false, data: copyStakersData },
     });
   },
 }));
