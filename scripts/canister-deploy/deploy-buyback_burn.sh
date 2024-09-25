d0ae200f68162d97fdf8cb6eea961c2dc0cafa67
Dustin Becker <dustin@bochslerfinance.com>
2024-09-25 21:26:57 +0200

    ci: update deploy-buyback_burn.sh script

--- scripts/canister-deploy/deploy-buyback_burn.sh
+++ scripts/canister-deploy/deploy-buyback_burn.sh
@@ -1,94 +1,150 @@
 #!/usr/bin/env bash
 
+NETWORK=$1
+
+DEPLOYMENT_VIA="direct"
 
 . ./scripts/extract_commit_tag_data_and_commit_sha.sh buyback_burn $NETWORK
 
-if [[ ! $1 =~ ^(local|staging|ic)$ ]]; then
-  echo "Error: unknown network for deployment"
-  exit 2
-fi
+if [[ $REINSTALL == "reinstall" ]]; then
 
+  if [[ $NETWORK =~ ^(local|staging)$ ]]; then
+    TESTMODE=true
+    GLDGOV_LEDGER_CANISTER_ID=$(dfx canister id --network ic sns_ledger)
+    AUTHORIZED_PRINCIPAL=465sx-szz6o-idcax-nrjhv-hprrp-qqx5e-7mqwr-wadib-uo7ap-lofbe-dae
+    # 11 days
+    BURN_INTERVAL_IN_SECS=$((11*24*3600))
+    # 6 hours
+    SWAP_INTERVAL_IN_SECS=$((6*3600))
+  elif [[ $NETWORK =~ ^(ic)$ ]]; then
+    TESTMODE=false
+    GLDGOV_LEDGER_CANISTER_ID=$(dfx canister id --network $NETWORK sns_ledger)
+    AUTHORIZED_PRINCIPAL=$(dfx canister id --network $NETWORK sns_governance)
+    # 6 hours
+    BURN_INTERVAL_IN_SECS=$((6*3600))
+    SWAP_INTERVAL_IN_SECS=$((6*3600))
+  else
+    echo "Error: unknown network for deployment. Found $NETWORK."
+    exit 2
+  fi
 
-if [[ $1 =~ ^(local|staging)$ ]]; then
-TESTMODE="true"
-# GLDGOV_LEDGER_CANISTER_ID=irhm6-5yaaa-aaaap-ab24q-cai
-GLDGOV_LEDGER_CANISTER_ID=tyyy3-4aaaa-aaaaq-aab7a-cai
-# SNS_GOVERNANCE_CANISTER_ID=j3ioe-7iaaa-aaaap-ab23q-cai
-AUTHORIZED_PRINCIPAL=465sx-szz6o-idcax-nrjhv-hprrp-qqx5e-7mqwr-wadib-uo7ap-lofbe-dae
-# 11 days
-BURN_INTERVAL_IN_SECS=1_000_000
-# 6 hours
-SWAP_INTERVAL_IN_SECS=21_600
+  export BURN_RATE=33
+  export MIN_ICP_BURN_AMOUNT=30_000_000_000
+  export ICP_SWAP_CANISTER_ID="7eikv-2iaaa-aaaag-qdgwa-cai"
+
+  ARGUMENTS="(variant { Init = record {
+    test_mode = $TESTMODE;
+    commit_hash = \"$COMMIT_SHA\";
+    authorized_principals = vec {
+      principal \"$AUTHORIZED_PRINCIPAL\";
+    };
+    icp_swap_canister_id = principal \"$ICP_SWAP_CANISTER_ID\";
+    gldgov_ledger_canister_id = principal \"$GLDGOV_LEDGER_CANISTER_ID\";
+    tokens = vec {};
+    burn_rate = $BURN_RATE : nat8;
+    min_icp_burn_amount = record { e8s = $MIN_ICP_BURN_AMOUNT : nat64 };
+    burn_interval_in_secs = $BURN_INTERVAL_IN_SECS : nat64;
+    swap_interval_in_secs = $SWAP_INTERVAL_IN_SECS : nat64;
+  }})"
+
 else
-TESTMODE="false"
-GLDGOV_LEDGER_CANISTER_ID=tyyy3-4aaaa-aaaaq-aab7a-cai
-# SNS_GOVERNANCE_CANISTER_ID=tr3th-kiaaa-aaaaq-aab6q-cai
-AUTHORIZED_PRINCIPAL=465sx-szz6o-idcax-nrjhv-hprrp-qqx5e-7mqwr-wadib-uo7ap-lofbe-dae
-# 6 hours
-BURN_INTERVAL_IN_SECS=21_600
-SWAP_INTERVAL_IN_SECS=21_600
+  ARGUMENTS="(variant { Upgrade = record {
+    wasm_version = $WASM_VERSION;
+    commit_hash = \"$COMMIT_SHA\";
+  }})"
 fi
 
-export BURN_RATE=33
-export MIN_ICP_BURN_AMOUNT=30_000_000_000
-export ICP_SWAP_CANISTER_ID=7eikv-2iaaa-aaaag-qdgwa-cai
+. ./scripts/deploy_backend_canister.sh buyback_burn $NETWORK "$ARGUMENTS" $DEPLOYMENT_VIA $VERSION $REINSTALL
 
-# FIXME
-export COMMIT_HASH=hello
 
-# Init args
-ARGS='(
-  variant {
-    Init = record {
-      test_mode = '"$TESTMODE"';
-      commit_hash = "'"$COMMIT_HASH"'";
-      authorized_principals = vec {
-        principal "'"$AUTHORIZED_PRINCIPAL"'";
-      };
-      icp_swap_canister_id = principal "'"$ICP_SWAP_CANISTER_ID"'";
-      gldgov_ledger_canister_id = principal "'"$GLDGOV_LEDGER_CANISTER_ID"'";
-      tokens = vec {};
-      burn_rate = '"$BURN_RATE"' : nat8;
-      min_icp_burn_amount = record { e8s = '"$MIN_ICP_BURN_AMOUNT"' : nat64 };
-      burn_interval_in_secs = '"$BURN_INTERVAL_IN_SECS"' : nat64;
-      swap_interval_in_secs = '"$SWAP_INTERVAL_IN_SECS"' : nat64;
-    }
-  }
-)'
+# . ./scripts/extract_commit_tag_data_and_commit_sha.sh buyback_burn $NETWORK
 
+# if [[ ! $1 =~ ^(local|staging|ic)$ ]]; then
+#   echo "Error: unknown network for deployment"
+#   exit 2
+# fi
+
+
+# if [[ $1 =~ ^(local|staging)$ ]]; then
+# TESTMODE="true"
+# # GLDGOV_LEDGER_CANISTER_ID=irhm6-5yaaa-aaaap-ab24q-cai
+# GLDGOV_LEDGER_CANISTER_ID=tyyy3-4aaaa-aaaaq-aab7a-cai
+# # SNS_GOVERNANCE_CANISTER_ID=j3ioe-7iaaa-aaaap-ab23q-cai
+# AUTHORIZED_PRINCIPAL=465sx-szz6o-idcax-nrjhv-hprrp-qqx5e-7mqwr-wadib-uo7ap-lofbe-dae
+# # 11 days
+# BURN_INTERVAL_IN_SECS=1_000_000
+# # 6 hours
+# SWAP_INTERVAL_IN_SECS=21_600
+# else
+# TESTMODE="false"
+# GLDGOV_LEDGER_CANISTER_ID=tyyy3-4aaaa-aaaaq-aab7a-cai
+# # SNS_GOVERNANCE_CANISTER_ID=tr3th-kiaaa-aaaaq-aab6q-cai
+# AUTHORIZED_PRINCIPAL=465sx-szz6o-idcax-nrjhv-hprrp-qqx5e-7mqwr-wadib-uo7ap-lofbe-dae
+# # 6 hours
+# BURN_INTERVAL_IN_SECS=21_600
+# SWAP_INTERVAL_IN_SECS=21_600
+# fi
+
+# export BURN_RATE=33
+# export MIN_ICP_BURN_AMOUNT=30_000_000_000
+# export ICP_SWAP_CANISTER_ID=7eikv-2iaaa-aaaag-qdgwa-cai
+
+# # FIXME
+# export COMMIT_HASH=hello
+
+# # Init args
 # ARGS='(
 #   variant {
-#     Upgrade = record {
-#       wasm_version = record {
-#         major = 0 : nat32;
-#         minor = 0 : nat32;
-#         patch = 1 : nat32;
-#       };
+#     Init = record {
+#       test_mode = '"$TESTMODE"';
 #       commit_hash = "'"$COMMIT_HASH"'";
+#       authorized_principals = vec {
+#         principal "'"$AUTHORIZED_PRINCIPAL"'";
+#       };
+#       icp_swap_canister_id = principal "'"$ICP_SWAP_CANISTER_ID"'";
+#       gldgov_ledger_canister_id = principal "'"$GLDGOV_LEDGER_CANISTER_ID"'";
+#       tokens = vec {};
+#       burn_rate = '"$BURN_RATE"' : nat8;
+#       min_icp_burn_amount = record { e8s = '"$MIN_ICP_BURN_AMOUNT"' : nat64 };
+#       burn_interval_in_secs = '"$BURN_INTERVAL_IN_SECS"' : nat64;
+#       swap_interval_in_secs = '"$SWAP_INTERVAL_IN_SECS"' : nat64;
 #     }
-# })'
+#   }
+# )'
 
-echo "Deployment arguments: \n" $ARGS
+# # ARGS='(
+# #   variant {
+# #     Upgrade = record {
+# #       wasm_version = record {
+# #         major = 0 : nat32;
+# #         minor = 0 : nat32;
+# #         patch = 1 : nat32;
+# #       };
+# #       commit_hash = "'"$COMMIT_HASH"'";
+# #     }
+# # })'
 
-# Deployment Logic
-if [[ $1 == "local" ]]; then
-  # Deploy to local environment
-  dfx deploy buyback_burn --network $1 ${REINSTALL} --argument "$ARGS" -y
-elif [[ $1 == "staging" ]]; then
-  # Deploy to staging environment - works both manually and from CI/CD
-  if [[ -n "$CI_COMMIT_REF_NAME" ]]; then
-    # This block runs in CI/CD
-    echo "Deploying to staging from CI/CD on branch: $CI_COMMIT_REF_NAME"
-    dfx deploy buyback_burn --network $1 ${REINSTALL} --argument "$ARGS" -y
-  else
-    # This block runs when deploying manually
-    echo "Deploying to staging manually"
-    dfx deploy buyback_burn --network $1 ${REINSTALL} --argument "$ARGS" -y
-  fi
-elif [[ $CI_COMMIT_REF_NAME == "develop" || ( $1 == "ic" && $CI_COMMIT_TAG =~ ^buyback_burn-v{1}[[:digit:]]{1,2}.[[:digit:]]{1,2}.[[:digit:]]{1,3}$ ) ]]; then
-  # Deploy to production (ic) environment if tag matches the pattern
-  dfx deploy buyback_burn --network $1 ${REINSTALL} --argument "$ARGS" -y
-else
-  echo "Error: Invalid deployment context or missing environment variables."
-  exit 4
-fi
+# echo "Deployment arguments: \n" $ARGS
+
+# # Deployment Logic
+# if [[ $1 == "local" ]]; then
+#   # Deploy to local environment
+#   dfx deploy buyback_burn --network $1 ${REINSTALL} --argument "$ARGS" -y
+# elif [[ $1 == "staging" ]]; then
+#   # Deploy to staging environment - works both manually and from CI/CD
+#   if [[ -n "$CI_COMMIT_REF_NAME" ]]; then
+#     # This block runs in CI/CD
+#     echo "Deploying to staging from CI/CD on branch: $CI_COMMIT_REF_NAME"
+#     dfx deploy buyback_burn --network $1 ${REINSTALL} --argument "$ARGS" -y
+#   else
+#     # This block runs when deploying manually
+#     echo "Deploying to staging manually"
+#     dfx deploy buyback_burn --network $1 ${REINSTALL} --argument "$ARGS" -y
+#   fi
+# elif [[ $CI_COMMIT_REF_NAME == "develop" || ( $1 == "ic" && $CI_COMMIT_TAG =~ ^buyback_burn-v{1}[[:digit:]]{1,2}.[[:digit:]]{1,2}.[[:digit:]]{1,3}$ ) ]]; then
+#   # Deploy to production (ic) environment if tag matches the pattern
+#   dfx deploy buyback_burn --network $1 ${REINSTALL} --argument "$ARGS" -y
+# else
+#   echo "Error: Invalid deployment context or missing environment variables."
+#   exit 4
+# fi
