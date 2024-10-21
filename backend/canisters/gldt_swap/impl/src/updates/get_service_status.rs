89db162bd2b618d63bffc83affb49763852097c9
Freddie Hands <freddie@daolink.ch>
2024-10-21 14:57:48 -0300

    chore: do not hide service status

--- backend/canisters/gldt_swap/impl/src/updates/get_service_status.rs
+++ backend/canisters/gldt_swap/impl/src/updates/get_service_status.rs
@@ -6,7 +6,6 @@ use ic_cdk::update;
 
 use crate::service_status::check_service_status;
 
-#[update(hidden = true)]
 async fn get_service_status(_: GetServiceStatusArgs) -> GetServiceStatusResponse {
     check_service_status().await
 }
