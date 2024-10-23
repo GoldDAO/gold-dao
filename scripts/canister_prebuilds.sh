28062c890218ce3c3c60b09e8f94bb348728d7e9
Freddie Hands <freddie@daolink.ch>
2024-10-23 09:33:57 -0300

    fix: pre build should use  arg instead of

--- scripts/canister_prebuilds.sh
+++ scripts/canister_prebuilds.sh
@@ -2,12 +2,13 @@
 
 CANISTER=$1
 BASE_CANISTER_PATH=$2
-INTTEST=$3
+INTTEST=$4
 
 echo "Running prebuild scripts for $CANISTER"
+echo "Flags passed $INTTEST"
 
 if [ "$CANISTER" == "gldt_swap" ]; then
-    cargo build --target wasm32-unknown-unknown --target-dir $BASE_CANISTER_PATH/gldt_swap_archive/target --release --locked $INTTEST -p gldt_swap_archive
+    cargo build --target wasm32-unknown-unknown --target-dir $BASE_CANISTER_PATH/gldt_swap_archive/target --release --locked --features $INTTEST -p gldt_swap_archive
     ic-wasm $BASE_CANISTER_PATH/gldt_swap_archive/target/wasm32-unknown-unknown/release/gldt_swap_archive.wasm -o $BASE_CANISTER_PATH/gldt_swap_archive/target/wasm32-unknown-unknown/release/gldt_swap_archive.wasm shrink
 	  ic-wasm $BASE_CANISTER_PATH/gldt_swap_archive/target/wasm32-unknown-unknown/release/gldt_swap_archive.wasm -o $BASE_CANISTER_PATH/gldt_swap_archive/target/wasm32-unknown-unknown/release/gldt_swap_archive.wasm optimize --inline-functions-with-loops O3
     gzip --no-name -9 -v -c $BASE_CANISTER_PATH/gldt_swap_archive/target/wasm32-unknown-unknown/release/gldt_swap_archive.wasm > $BASE_CANISTER_PATH/gldt_swap_archive/target/wasm32-unknown-unknown/release/gldt_swap_archive_canister.wasm.gz &&
