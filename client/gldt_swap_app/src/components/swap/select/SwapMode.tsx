d973e66ede531ef35743ae4abf15237a5be47ef0
aditsch <audran@daolink.ch>
2024-10-29 10:56:24 +0100

    update logos

--- client/gldt_swap_app/src/components/swap/select/SwapMode.tsx
+++ client/gldt_swap_app/src/components/swap/select/SwapMode.tsx
@@ -5,9 +5,7 @@ import { LogoGLDT } from "@components/shared/logos";
 const SWAP_MODE_OPTIONS = [
   {
     value: "GLD NFT",
-    icon: (
-      <img src="gold-bars/1g.svg" className="w-4 h-4" alt="Gold bar logo" />
-    ),
+    icon: <img src="/nft_logo.svg" className="w-4 h-4" alt="Gold bar logo" />,
     label: "NFT",
   },
   { value: "GLDT", icon: <LogoGLDT className="w-4 h-4" />, label: "ICRC-1" },
