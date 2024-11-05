eb8a070daceb92907fc5f85a08248f06d6106009
Léo <leo@sisyphe.dev>
2024-11-05 12:10:36 +0100

    some numeric types

--- backend/canisters/usdg/impl/src/lib.rs
+++ backend/canisters/usdg/impl/src/lib.rs
@@ -1,9 +1,12 @@
 pub mod lifecycle;
 pub mod management;
+pub mod numeric;
 pub mod queries;
 pub mod state;
 pub mod tasks;
 pub mod updates;
+
+pub const E8S: u64 = 100_000_000;
 
 /// Time constants
 const SEC_NANOS: u64 = 1_000_000_000;
