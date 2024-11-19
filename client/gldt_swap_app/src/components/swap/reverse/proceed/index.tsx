d363aa1da0049a2c8fa73a11660c7f228b91121b
aditsch <audran@daolink.ch>
2024-11-19 10:25:17 +0100

    improve onError reverse swap

--- client/gldt_swap_app/src/components/swap/reverse/proceed/index.tsx
+++ client/gldt_swap_app/src/components/swap/reverse/proceed/index.tsx
@@ -48,6 +48,9 @@ const Proceed = () => {
     queryClient.invalidateQueries({
       queryKey: ["FETCH_AVAILABLE_NFTS"],
     });
+    queryClient.invalidateQueries({
+      queryKey: ["USER_FETCH_BALANCE_GLDT"],
+    });
     setCanCloseDialog(true);
   };
 
@@ -59,6 +62,7 @@ const Proceed = () => {
       },
       onError: (err) => {
         console.error(err);
+        reset();
       },
     });
   };
