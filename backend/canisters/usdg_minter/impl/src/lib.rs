0fc01e9cbcf3329b54f351055ddd00fb410461d8
Léo <leo@sisyphe.dev>
2024-11-21 09:50:32 +0100

    fix

--- backend/canisters/usdg_minter/impl/src/lib.rs
+++ backend/canisters/usdg_minter/impl/src/lib.rs
@@ -1,4 +1,10 @@
 use crate::numeric::{Factor, GoldPrice, GLDT};
+use ic_cdk::export_candid;
+use icrc_ledger_types::icrc1::account::Account;
+use usdg_minter_api::lifecycle::MinterArgument;
+use usdg_minter_api::updates::open_vault::{OpenVaultArg, OpenVaultSuccess};
+use usdg_minter_api::ApiVault;
+use usdg_minter_api::VaultError;
 
 pub mod cbor;
 pub mod lifecycle;
@@ -22,3 +28,5 @@ const MINIMUM_MARGIN_AMOUNT: GLDT = GLDT::from_unscaled(50);
 pub const DEFAULT_GOLD_PRICE: GoldPrice = GoldPrice::from_e8s(8_300_000);
 
 pub const MINIMUM_COLLATERAL_RATIO: Factor = Factor::from_e8s(105_000_000);
+
+export_candid!();
