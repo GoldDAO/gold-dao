d790a708a43ecaab55e1c52cad112e111444b52c
Dustin Becker <dustin.becker@bity.com>
2025-05-19 21:00:48 +0200

    hotfix: payload of dex_transfer_position_validate

--- backend/canisters/management/api/src/queries/dex_transfer_position_validate.rs
+++ backend/canisters/management/api/src/queries/dex_transfer_position_validate.rs
@@ -1,4 +1,6 @@
 use candid::Principal;
 
-pub type Args = (Principal, Principal, u64);
+pub type Arg1 = Principal;
+pub type Arg2 = Principal;
+pub type Arg3 = u64;
 pub type Response = Result<String, String>;
