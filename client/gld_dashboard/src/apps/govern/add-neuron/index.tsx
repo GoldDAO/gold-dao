e2c7bd90cbcfba0fd4affb8d620defbb736a8781
Audran Ditsch <audran.ditsch@bity.com>
2025-06-17 11:42:52 +0000

    Resolve "Unclear instructions to claim GOLDAO neurons rewards"

--- client/gld_dashboard/src/apps/govern/add-neuron/index.tsx
+++ client/gld_dashboard/src/apps/govern/add-neuron/index.tsx
@@ -1,8 +1,7 @@
 import { ReactNode } from "react";
-import { useAtom } from "jotai";
 import { useAuth } from "@auth/index";
 import { ExternalLink } from "@components/index";
-import { AddNeuronStateReducerAtom } from "./atoms";
+import Dialog from "@components/dialogs/Dialog";
 import Address from "@components/strings/Address";
 import BtnPrimary from "@shared/components/ui/button/BtnPrimary";
 
@@ -25,54 +24,73 @@ Card.Text = ({ children }: { children: ReactNode }) => {
   return <div className="text-content/60">{children}</div>;
 };
 
-const AddNeuron = () => {
+const AddNeuron = ({
+  open,
+  handleClose,
+}: {
+  open: boolean;
+  handleClose: () => void;
+}) => {
   const { principalId } = useAuth();
-  const [, dispatch] = useAtom(AddNeuronStateReducerAtom);
 
   return (
-    <>
-      <div className="text-center mt-4 mb-6 text-4xl xl:text-5xl">
-        Add <span className="font-semibold text-gold">Neuron</span>
+    <Dialog open={open} handleOnClose={handleClose}>
+      <div className="mt-4 mb-6 px-4 xl:px-8">
+        <div className="text-center mb-2 text-4xl xl:text-5xl">
+          Add <span className="font-semibold text-gold">Neuron</span>
+        </div>
+        <div className="flex justify-center">
+          <div className="text-content/60 text-center">
+            In order to add a neuron to the Gold DAO dApp, you need to own a
+            GOLDAO neuron. Follow the steps described in{" "}
+            <ExternalLink
+              href="https://docs.gold-dao.org/how-to/stake-the-goldao-tokens"
+              className="text-copper"
+            >
+              this guide
+            </ExternalLink>{" "}
+            in order to obtain GOLDAO tokens and stake them in a GOLDAO neuron.
+          </div>
+        </div>
+
+        <div className="my-8 grid grid-cols-1 gap-4">
+          <Card step={1}>
+            <Card.Title>Copy you principal from the Gold DAO dApp</Card.Title>
+            <Card.Text>
+              <Address size="auto">{principalId}</Address>
+            </Card.Text>
+          </Card>
+          <Card step={2}>
+            <Card.Title>
+              Login to the{" "}
+              <span>
+                <ExternalLink href="https://nns.ic0.app/neurons/?u=tw2vt-hqaaa-aaaaq-aab6a-cai">
+                  NNS dApp
+                </ExternalLink>
+              </span>
+            </Card.Title>
+            <Card.Text>to manage your Gold DAO neurons</Card.Text>
+          </Card>
+          <Card step={3}>
+            <Card.Title>Add your principal as hotkey to each neuron</Card.Title>
+            <Card.Text>
+              Link your neurons by adding your previously copied principal as a
+              "hotkey" to each of your neurons
+            </Card.Text>
+          </Card>
+          <Card step={4}>
+            <Card.Title>It's done!</Card.Title>
+            <Card.Text>
+              Refresh your Gold DAO dApp and you will now see your neuron
+              linked.
+            </Card.Text>
+          </Card>
+        </div>
+        <BtnPrimary className="w-full" onClick={handleClose}>
+          Got it!
+        </BtnPrimary>
       </div>
-      <div className="my-8 grid grid-cols-1 gap-4">
-        <Card step={1}>
-          <Card.Title>Copy you principal from the Gold DAO dApp</Card.Title>
-          <Card.Text>
-            <Address size="auto">{principalId}</Address>
-          </Card.Text>
-        </Card>
-        <Card step={2}>
-          <Card.Title>
-            Login to the{" "}
-            <span>
-              <ExternalLink href="https://nns.ic0.app/neurons/?u=tw2vt-hqaaa-aaaaq-aab6a-cai">
-                NNS dApp
-              </ExternalLink>
-            </span>
-          </Card.Title>
-          <Card.Text>to manage your Gold DAO neurons</Card.Text>
-        </Card>
-        <Card step={3}>
-          <Card.Title>Add your principal as hotkey to each neuron</Card.Title>
-          <Card.Text>
-            Link your neurons by adding your previously copied principal as a
-            "hotkey" to each of your neurons
-          </Card.Text>
-        </Card>
-        <Card step={4}>
-          <Card.Title>It's done!</Card.Title>
-          <Card.Text>
-            Refresh your Gold DAO dApp and you will now see your neuron linked.
-          </Card.Text>
-        </Card>
-      </div>
-      <BtnPrimary
-        className="w-full"
-        onClick={() => dispatch({ type: "RESET" })}
-      >
-        Got it!
-      </BtnPrimary>
-    </>
+    </Dialog>
   );
 };
 
