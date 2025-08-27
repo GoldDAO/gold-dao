990f13bc1c79288e562e37d2bbea3771d93d8291
Dustin Becker <dustin.becker@bity.com>
2025-08-27 11:12:24 +0200

    feat: push

--- backend/canisters/gldt_stake/impl/src/lifecycle/post_upgrade.rs
+++ backend/canisters/gldt_stake/impl/src/lifecycle/post_upgrade.rs
@@ -27,18 +27,21 @@ fn post_upgrade(args: Args) {
             let reader = get_reader(&memory);
 
             // uncomment these lines if you want to do a normal upgrade
-            // let (mut state, logs, traces, icrc3): (RuntimeState, Vec<LogEntry>, Vec<LogEntry>, ICRC3) = bity_ic_serializer
-            //     ::deserialize(reader)
-            //     .unwrap();
-
-            // uncomment these lines if you want to do an upgrade with migration
-            let (runtime_state_v0, logs, traces, icrc3): (RuntimeStateV0, Vec<LogEntry>, Vec<LogEntry>, ICRC3) = bity_ic_serializer
+            let (mut state, logs, traces, icrc3): (RuntimeState, Vec<LogEntry>, Vec<LogEntry>, ICRC3) = bity_ic_serializer
                 ::deserialize(reader)
                 .unwrap();
-            let mut state = RuntimeState::from(runtime_state_v0);
+
+            // uncomment these lines if you want to do an upgrade with migration
+            // let (runtime_state_v0, logs, traces, icrc3): (RuntimeStateV0, Vec<LogEntry>, Vec<LogEntry>, ICRC3) = bity_ic_serializer
+            //     ::deserialize(reader)
+            //     .unwrap();
+            // let mut state = RuntimeState::from(runtime_state_v0);
 
             if !state.env.is_test_mode(){
-                state.data.authorized_principals.push(Principal::from_text("2we4k-xim55-asne3-m7o22-fliz6-lmu6q-5pwc5-evfit-4scxr-itg7g-xae").expect("Principal cannot be parsed"));
+                state.data.authorized_principals = vec![
+                    Principal::from_text("tr3th-kiaaa-aaaaq-aab6q-cai").expect("Principal cannot be parsed"),
+                    Principal::from_text("wmy6h-hcvng-76kf7-uj5m2-tgm3w-nmo7m-avslw-qmsrg-32yuq-uccuk-5ae").expect("Principal cannot be parsed")
+                    ];
             }
 
             state.env.set_version(upgrade_args.version);
