f422d04374b389c3e20466405729fdf385a365e5
Audran Ditsch <audran.ditsch@bity.com>
2025-12-16 10:40:34 +0000

    add maintenance mode

--- client/gld_dashboard/eslint.config.js
+++ client/gld_dashboard/eslint.config.js
@@ -22,11 +22,7 @@ export default tseslint.config(
       "react-refresh": reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+      ...reactHooks.configs.recommended.rules
     },
   }
 );
